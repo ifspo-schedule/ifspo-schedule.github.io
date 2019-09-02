74ab6006dc2ab1bb80dd6e5644d64e2b06baf467
ifspo-schedule <54813491+ifspo-schedule@users.noreply.github.com>
2019-09-02 21:59:32 +0300

    Add files via upload

--- k.js
+++ k.js
@@ -20,7 +20,7 @@ document.querySelector('#chet').addEventListener("click", () => {
     document.querySelector('#chet').className = "btn_blue__active";
     document.querySelector('#nechet').className = "btn_blue";
     document.querySelector('#all').className = "btn_blue";
-})
+});
 
 document.querySelector('#nechet').addEventListener("click", () => {
     document.querySelector('#nechet_block').className = "display_block_border_none";
@@ -30,7 +30,7 @@ document.querySelector('#nechet').addEventListener("click", () => {
     document.querySelector('#chet').className = "btn_blue";
     document.querySelector('#nechet').className = "btn_blue__active";
     document.querySelector('#all').className = "btn_blue";
-})
+});
 
 document.querySelector('#all').addEventListener("click", () => {
     document.querySelector('#nechet_block').className = "display_block_border_none";
@@ -41,4 +41,35 @@ document.querySelector('#all').addEventListener("click", () => {
     document.querySelector('#nechet').className = "btn_blue";
     document.querySelector('#chet').className = "btn_blue";
 
-})
+});
+
+
+Date.prototype.getWeek = function () {
+    var target = new Date(this.valueOf());
+    var dayNr = (this.getDay() + 6) % 7;
+    target.setDate(target.getDate() - dayNr + 3);
+    var firstThursday = target.valueOf();
+    target.setMonth(0, 1);
+    if (target.getDay() != 4) {
+        target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7);
+    }
+    return 1 + Math.ceil((firstThursday - target) / 604800000);
+}
+
+var DW = document.querySelector('#week'),
+    NW = document.querySelector('#week ~ output'),
+    NWG = document.querySelector('#week ~ output ~ output');
+
+DW.value = new Date().toJSON().slice(0, 10); // сегодняшняя дата в формате yyyy-mm-dd для input type="date"
+
+NW.innerHTML = new Date(DW.value).getWeek(); // номер текущей недели
+
+if (new Date(DW.value).getWeek() % 2 == 0) {
+    document.querySelector('.chetnost').textContent = "Нечётная неделя";
+}
+if (new Date(DW.value).getWeek() % 2 == 1) {
+    document.querySelector('.chetnost').textContent = "Чётная неделя";
+}
+
+
+NWG.innerHTML = new Date(new Date(DW.value).getFullYear(), 11, 28).getWeek();
