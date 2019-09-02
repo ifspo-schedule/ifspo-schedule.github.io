7a5a5e8f7ed4181ce8bc56bb8a2ca5bec61a9e8e
ifspo-schedule <54813491+ifspo-schedule@users.noreply.github.com>
2019-09-02 19:38:46 +0300

    Add files via upload

--- k.js
+++ k.js
@@ -14,7 +14,7 @@ document.querySelector('#date').textContent = (day[d.getDay()] + " " + d.getDate
 
 document.querySelector('#chet').addEventListener("click", () => {
     document.querySelector('#nechet_block').className = "display_none";
-    document.querySelector('#chet_block').className = "display_block";
+    document.querySelector('#chet_block').className = "display_block_border_none";
     document.querySelector('#chet_blocktwo').className = "display_block_border_none";
     document.querySelector('#bottom').className = "chet-rects-block-lessons-block";
     document.querySelector('#chet').className = "btn_blue__active";
@@ -23,7 +23,7 @@ document.querySelector('#chet').addEventListener("click", () => {
 })
 
 document.querySelector('#nechet').addEventListener("click", () => {
-    document.querySelector('#nechet_block').className = "display_block";
+    document.querySelector('#nechet_block').className = "display_block_border_none";
     document.querySelector('#chet_block').className = "display_none";
     document.querySelector('#chet_blocktwo').className = "display_none";
     document.querySelector('#bottom').className = "chet-rects-block-lessons-block-bottom";
@@ -33,11 +33,12 @@ document.querySelector('#nechet').addEventListener("click", () => {
 })
 
 document.querySelector('#all').addEventListener("click", () => {
-    document.querySelector('#nechet_block').className = "display_block";
+    document.querySelector('#nechet_block').className = "display_block_border_none";
     document.querySelector('#chet_block').className = "display_block";
     document.querySelector('#chet_blocktwo').className = "display_block_border_none";
     document.querySelector('#bottom').className = "chet-rects-block-lessons-block";
     document.querySelector('#all').className = "btn_blue__active";
     document.querySelector('#nechet').className = "btn_blue";
     document.querySelector('#chet').className = "btn_blue";
+
 })
